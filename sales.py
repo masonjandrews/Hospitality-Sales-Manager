3a1a6d3b8151e62c153dcffde81d66f7016ad448
Mason Andrews <masonandrews2000@gmail.com>
2022-11-03 20:40:44 +0000

    test csv output with date

--- sales.py
+++ sales.py
@@ -1,9 +1,12 @@
 import csv
+import datetime
+
+iDate = datetime.datetime.now()
+date = iDate.strftime("%x")
 
 with open('sales.csv', mode='w') as sales:
     sales_writer = csv.writer(sales, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     sales_writer.writerow(['Date', 'Gross', 'Net'])
-
-    
+    sales_writer.writerow([date, date, date])
     
