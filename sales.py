f79f0a1a863790d4912ba2bc4434a6eed072d669
Mason Andrews <masonandrews2000@gmail.com>
2022-11-03 20:56:18 +0000

    add gross sales input

--- sales.py
+++ sales.py
@@ -4,9 +4,10 @@ import datetime
 iDate = datetime.datetime.now()
 date = iDate.strftime("%x")
 
+gross = int(input("Please enter todays gross sales:"))
+
 with open('sales.csv', mode='w') as sales:
     sales_writer = csv.writer(sales, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     sales_writer.writerow(['Date', 'Gross', 'Net'])
-    sales_writer.writerow([date, date, date])
-    
+    sales_writer.writerow([date, gross, ])
