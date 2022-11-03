1d95507bc75086225900932f30ed79fcd826646f
Mason Andrews <masonandrews2000@gmail.com>
2022-11-03 23:38:05 +0000

    Update sales.py
    Add comments, define tax rates as variable, testing basic net calculations

--- sales.py
+++ sales.py
@@ -1,13 +1,18 @@
-import csv
-import datetime
+import csv #Import csv to allow working with csv files
+import datetime #Import datetime to enable working with dates
 
 iDate = datetime.datetime.now()
-date = iDate.strftime("%x")
+date = iDate.strftime("%x") #Create date variable and store it in MM/DD/YYYY format
 
-gross = int(input("Please enter todays gross sales:"))
+gross = int(input("Please enter todays gross sales:")) #Create gross sales variable from user input
 
-with open('sales.csv', mode='w') as sales:
-    sales_writer = csv.writer(sales, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+tax = 1.2 #Define the tax rate, set as variable so it can be changed accordingly
 
-    sales_writer.writerow(['Date', 'Gross', 'Net'])
-    sales_writer.writerow([date, gross, ])
+net = gross / tax #Work out the net sales by dividing by the tax rate
+
+with open('sales.csv', mode='w') as sales: #Create sales.csv and open it
+    sales_writer = csv.writer(sales, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL) #Define the writing algorithm
+
+    sales_writer.writerow(['Date', 'Gross', 'Net']) #Write headings into file for readability
+    sales_writer.writerow(['00/00/0000', '0', '0']) #Zero values to initialize the calculations
+    sales_writer.writerow([date, gross, ]) #Write data from program into file
