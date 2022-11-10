de43d7eb97b1d04e3765d22a3d1e6e760b309007
Mason Andrews <masonandrews2000@gmail.com>
2022-11-10 19:57:31 +0000

    Update sales.py

--- sales.py
+++ sales.py
@@ -13,6 +13,5 @@ net = gross / tax #Work out the net sales by dividing by the tax rate
 with open('sales.csv', mode='a') as sales: #Create sales.csv and open it
     sales_writer = csv.writer(sales, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL) #Define the writing algorithm
 
-    sales_writer.writerow(['Date', 'Gross', 'Net']) #Write headings into file for readability
     sales_writer.writerow(['00/00/0000', '0', '0']) #Zero values to initialize the calculations
     sales_writer.writerow([date, gross, ]) #Write data from program into file
